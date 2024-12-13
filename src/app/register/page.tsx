f629829aac0dcfa44349c196cb102c90c7942347
EngJao89 <eng_joao@outlook.com>
2024-12-12 21:35:04 -0300

    feat(bth05-info-register): implements infos of register new users

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,11 +1,27 @@
 import Image from "next/image";
+import { RxArrowLeft } from "react-icons/rx";
+
 import logo from "../../../public/Logo.png"
+import { Button } from "@/components/ui/button";
 
 export default function Register() {
   return (
     <div className="flex justify-around items-center h-screen">
       <div>
-        <Image src={logo} alt="Logo"/>
+        <div>
+          <Image src={logo} alt="Logo"/>
+        </div>
+        <div className="mt-16">
+          <h5 className="text-zinc-950 font-bold text-xl">Cadastro</h5>
+          <h3 className="text-zinc-400 text-xs mt-6">
+            Faça seu cadastro, entre na plataforma e ajude<br/> pessoas a encontrarem os casos da sua ONG.
+          </h3>
+
+          <Button variant="ghost" className="w-full mt-20 font-bold text-sm">
+            <RxArrowLeft color="red" size={24}/>
+            Voltar para o Logon
+          </Button>
+        </div>
       </div>
     </div>
   )
